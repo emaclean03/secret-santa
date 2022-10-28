105d6ad0c7f24ce7701014a9f42098558df80bae
Eric <>
2022-10-28 12:42:29 -0400

    Fixing typo

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -4,7 +4,7 @@
       <div class="w-3/4 mx-auto border-1 border-gray-200 rounded-lg">
         <div class="text-center mt-4" v-if="lists.length === 0">
           <h5><strong>You do not have any mystery lists!</strong>
-          <div><small>*Create a new on the right</small></div></h5>
+          <div><small>*Create a new Mystery list on the right</small></div></h5>
         </div>
         <List :lists="lists"/>
       </div>
